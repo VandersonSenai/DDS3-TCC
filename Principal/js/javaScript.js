5af99d80c19a530d9461a0c3db925d16522b63a7
kallangos <vandersondasilva@gmail.com>
2025-03-10 17:15:54 -0300

    Tratando Menus e SubMenus

--- Principal/js/javaScript.js
+++ Principal/js/javaScript.js
@@ -4,9 +4,19 @@ const sidebar = document.getElementById('sidebar')
 function toggleSubMenu(button){
   button.nextElementSibling.classList.toggle('show');
   button.classList.toggle('rotate');
+
+  if(sidebar.classList.contains('close')){
+    sidebar.classList.toggle('close')
+    toggleButton.classList.toggle('rotate')
+  }  
 }
 
 function toggleSidebar(){
   sidebar.classList.toggle('close');
   toggleButton.classList.toggle('rotate');
+
+  Array.from(sidebar.getElementsByClassName('show')).forEach(ul =>{
+    ul.classList.remove('show');
+    ul.prevElementSibling.classList.remove('rotate');
+  });
 }
