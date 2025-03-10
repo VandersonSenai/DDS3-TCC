f649ee556c8990aaa40e9bf5fca4b597876f47e4
kallangos <vandersondasilva@gmail.com>
2025-03-10 17:59:03 -0300

    Criando alternancia entre os botoes com submenus

--- Principal/js/javaScript.js
+++ Principal/js/javaScript.js
@@ -1,20 +1,34 @@
 const toggleButton = document.getElementById('toggle-btn')
 const sidebar = document.getElementById('sidebar')
 
-function toggleSubMenu(button){
-  button.nextElementSibling.classList.toggle('show');
-  button.classList.toggle('rotate');
-
-  if(sidebar.classList.contains('close')){
-    sidebar.classList.toggle('close')
-    toggleButton.classList.toggle('rotate')
-  }  
-}
-
 function toggleSidebar(){
   sidebar.classList.toggle('close');
   toggleButton.classList.toggle('rotate');
 
+  // Array.from(sidebar.getElementsByClassName('show')).forEach(ul =>{
+  //   ul.classList.remove('show');
+  //   ul.prevElementSibling.classList.remove('rotate');
+  // });
+  closeAllSubMenus() // substituindo Array.form acima por uma funcão.
+}
+
+function toggleSubMenu(button){
+
+  if(!button.nextElementSibling.classList.contains('show')){
+    closeAllSubMenus()
+  }
+  
+  button.nextElementSibling.classList.toggle('show');
+  button.classList.toggle('rotate');
+
+  if(sidebar.classList.contains('close')){
+    sidebar.classList.toggle('close');
+    toggleButton.classList.toggle('rotate');
+  }  
+}
+
+
+function closeAllSubMenus(){
   Array.from(sidebar.getElementsByClassName('show')).forEach(ul =>{
     ul.classList.remove('show');
     ul.prevElementSibling.classList.remove('rotate');
