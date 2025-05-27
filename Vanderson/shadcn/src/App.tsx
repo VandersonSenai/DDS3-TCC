7e0124627a7a7ba9fc2f9128cb129cfbc56cca3e
VandersonSenai <vanderson.silva13@aluno.senai.br>
2025-05-27 14:27:21 -0300

    ShadCN init

---
+++ Vanderson/shadcn/src/App.tsx
@@ -0,0 +1,14 @@
+// import './App.css'
+// import { Button } from "@/components/ui/button"
+import { AppSidebar } from "./components/app-sidebar"
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <AppSidebar />
+
+    </div>
+  )
+}
+
+export default App
